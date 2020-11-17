632c1b5a14b6fc77bee305d150a7ca4cb2115352
Wanderson M <wandersonmaceds@gmail.com>
2020-11-17 13:25:29 -0300

    criando recomendação básica de tópicos para os usuários

---
+++ src/recommendator/TopicRecommendator.js
@@ -0,0 +1,31 @@
+export default class TopicRecommendator {
+    
+    constructor(users, topics, limit = 10) {
+        this.users = users;
+        this.topics = topics;
+        this.limit = limit;
+    }
+
+    run() {
+        const usersTopicsRecommendations = this.users.map(user => {
+            const userCoursesIds = this._getCoursesIdsFromUser(user)
+            const recommendations = this._filterTopicsFromCoursesIds(userCoursesIds)
+
+            return {
+                user,
+                recommendations: recommendations.splice(0, this.limit)
+            };
+        });
+
+        return usersTopicsRecommendations;
+    }
+
+    _filterTopicsFromCoursesIds(userCoursesIds) {
+        return this.topics
+            .filter(topic => userCoursesIds.includes(topic.courseId));
+    }
+
+    _getCoursesIdsFromUser(user) {
+        return user.courses.map(course => course.courseId);
+    }
+}
